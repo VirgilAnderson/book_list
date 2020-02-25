2e0e82d8bcb5bf22a1cd6ef5db30c3e67ab8b64e
Virgil Anderson <virgil@Admins-MacBook-Pro.local>
2020-02-24 23:41:01 -0700

    Feat: created the book show view & controller

---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title')
+    <div>
+        <h1>{{ $book->title }}</h1>
+       </div>
+@endsection
+
+@section('buttons')
+    <a class="btn btn-outline-primary btn-sm" href="{{ route('books.index') }}" role="button">&laquo; Return to list</a>
+@endsection
+
+@section('content')
+<dl class="row">
+    <dt class="col-sm-3">Title</dt>
+    <dd class="col-sm-9">{{ $book->title }}</dd>
+
+    <dt class="col-sm-3">Author</dt>
+    <dd class="col-sm-9">{{ $book->author }}</dd>
+
+    <dt class="col-sm-3">Pages</dt>
+    <dd class="col-sm-9">{{ $book->pages }}</dd>
+
+    <dt class="col-sm-3">Genre</dt>
+    <dd class="col-sm-9">{{ $book->genre }}</dd>
+
+    <dt class="col-sm-3">Publisher</dt>
+    <dd class="col-sm-9">{{ $book->publisher }}</dd>
+
+    <dt class="col-sm-3">Description</dt>
+    <dd class="col-sm-9">{{ $book->description }}</dd>
+
+</dl>
+@endsection
