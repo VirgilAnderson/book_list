2b60d8e7905099423b514e019ae0ecce87ed63c9
Virgil Anderson <virgil@Admins-MacBook-Pro.local>
2020-02-27 00:51:03 -0700

    Feat: Added login/register buttons to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,9 @@
     <section class="text-white">
         <h1>Welcome To Book List App</h1>
         <h2>Register to create a list of books to read</h2>
+        <div class="btn-group">
+          <a href="{{ route('register') }}"type="button" class="btn btn-primary">Register</a>
+          <a href="{{ route('login') }}" type="button" class="btn btn-primary">Login</a>
+        </div>
     </section>
 @endsection
