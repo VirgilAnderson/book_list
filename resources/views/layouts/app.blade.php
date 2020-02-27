d22e595d564cdb17930b30ce41ed5ac84a28e97f
Virgil Anderson <virgil@Admins-MacBook-Pro.local>
2020-02-26 22:40:09 -0700

    Feat: Added conditionaly links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,12 @@
 								@endif
 							@else
 								<li class="nav-item">
+									<a class="nav-link" href="{{ route('home')}}">Dashboard</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="{{ route('books.index')}}">My List</a>
+								</li>
+								<li class="nav-item">
 									<a class="nav-link" href="{{ route('logout') }}"
 													onclick="event.preventDefault();
 													document.getElementById('logout-form').submit();">
